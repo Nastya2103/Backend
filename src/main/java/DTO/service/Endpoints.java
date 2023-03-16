0f9e6dfa8b6cc38b632c6ca7617852d2c189f7bd
Nastya2103 <korobeynikova2103@mail.ru>
2023-03-16 22:07:04 +0300

    Практическая работа 4

---
+++ src/main/java/DTO/service/Endpoints.java
@@ -0,0 +1,10 @@
+package DTO.service;
+
+public final class Endpoints {
+
+    public static final String getSearchRecipes = "recipes/complexSearch";
+
+    public static final String postClassifyCuisine = "recipes/cuisine";
+
+    public static final String mealPlan = "https://api.spoonacular.com/mealplanner/nastya_2103/items/";
+}
